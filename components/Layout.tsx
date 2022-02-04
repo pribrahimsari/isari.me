8ab0df03954ff55a25a38f12b3a2bc47237d30af
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-05 01:30:20 +0300

    typesafe: type definition for props of layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import {ReactChild, ReactFragment, ReactPortal} from "react";
 
-const Layout = (props) => {
+const Layout = (props: {children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined}) => {
     return (
         <div className="site-wrapper">
             <Header />
