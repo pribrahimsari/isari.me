bd067ba78ae6de94b0e14c086dfda1b751820332
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-05 03:43:29 +0300

    main css styles & svg logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
     return (
         <div className="logo">
             <Link href="/">
-                <a>İS</a>
+                <a>
+                    <Image src="/logo_is.svg" alt="logo is" width={120} height={120} />
+                </a>
             </Link>
         </div>
     );
