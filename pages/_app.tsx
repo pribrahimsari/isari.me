aeb3b9716b7cd9371f4003ebd8208f1ddeeb9ac3
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-06 07:07:56 +0300

    add vanta js & three, test in a div

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/globals.css";
 import type {AppProps} from "next/app";
 import {ThemeProvider} from "next-themes";
+import {useEffect} from "react";
 
 function MyApp({Component, pageProps}: AppProps) {
+    useEffect(() => {
+        const threeScript = document.createElement("script");
+        threeScript.setAttribute("id", "threeScript");
+        threeScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js");
+        document.getElementsByTagName("head")[0].appendChild(threeScript);
+        return () => {
+            if (threeScript) {
+                threeScript.remove();
+            }
+        };
+    }, []);
     return (
         <ThemeProvider defaultTheme="light">
             <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,39 @@ import type {NextPage} from "next";
 import Layout from "../components/Layout";
 import styles from "../styles/Index.module.css";
 import Link from "next/link";
+import {useEffect, useRef, useState} from "react";
+import * as THREE from "three";
+//@ts-ignore //todo: vanta type definitions????
+import CLOUDS from "vanta/dist/vanta.clouds.min";
 
 const Home: NextPage = () => {
+    const [vantaEffect, setVantaEffect] = useState(0);
+    const vantaRef = useRef(null);
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(
+                CLOUDS({
+                    el: vantaRef.current,
+                    THREE,
+                    /*
+                    mouseControls: true,
+                    touchControls: true,
+                    gyroControls: false,
+                    minHeight: 200.00,
+                    minWidth: 200.00
+                    * */
+                }),
+            );
+        }
+        return () => {
+            //@ts-ignore
+            if (vantaEffect) vantaEffect.destroy();
+        };
+    }, [vantaEffect]);
     return (
         <Layout>
             <section>
+                <div style={{width: 500, height: 500}} ref={vantaRef}></div>
                 <h1>Hi, I'm İbrahim.</h1>
                 <h2>Web Developer based on Istanbul, TURKEY</h2>
                 <img src="https://via.placeholder.com/350x470" alt="my picture" />
