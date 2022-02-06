b451f96e328f94c1dc91c86e7753fc297cf6caaf
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-07 02:58:24 +0300

    works page dummy card style

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,15 +1,57 @@
 import type {NextPage} from "next";
 import Layout from "../components/Layout";
 import styles from "../styles/Works.module.css";
+import stylesCard from "../styles/Workscard.module.css";
 import Head from "next/head";
 
 const Works: NextPage = () => {
+    const myWorks = [
+        {
+            title: "",
+            year: "",
+            headerImage: "",
+            techs: ["", ""],
+            description: "",
+            screenShots: ["", ""],
+            currentLink: "",
+            webArchiveLink: "",
+            mobileLink: "",
+        },
+    ];
+
     return (
         <Layout>
             <Head>
                 <title>My Works | İbrahim SARI</title>
             </Head>
-            <h1>Works</h1>
+            <div className={styles.worksWrapper}>
+                <h1>Works</h1>
+                <div className={stylesCard.container}>
+                    <div className={stylesCard.card}>
+                        <div className={stylesCard.cardHeader}>
+                            <img src="https://via.placeholder.com/1280x720" alt="rover" />
+                            <span className={`${stylesCard.yearTag} ${stylesCard.tag} ${stylesCard.tagBlue}`}>
+                                2010
+                            </span>
+                        </div>
+                        <div className={stylesCard.cardBody}>
+                            <div className={stylesCard.tagWrapper}>
+                                <span className={`${stylesCard.tag} ${stylesCard.tagTeal}`}>PHP</span>
+                                <span className={`${stylesCard.tag} ${stylesCard.tagPink}`}>PHP</span>
+                                <span className={`${stylesCard.tag} ${stylesCard.tagPurple}`}>PHP</span>
+                            </div>
+                            <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
+                            <p>An exploration into the truck's polarising design</p>
+                            <div className={stylesCard.buttons}>
+                                <button className="btn">View Clone</button>
+                                <button className="btn">Web Archive</button>
+                                <button className="btn">Mobile</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             {/*
             todo:
             Works bölümü filter şeklinde olmalı  ALL | Web | PHP (?) | Design
