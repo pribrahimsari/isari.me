0427976dfd380b575415133e5201fff64cca2bda
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-08 04:34:05 +0300

    sendmail and api key is working

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -1,29 +1,30 @@
 import sendgrid from "@sendgrid/mail";
 
-//todo: cannot get sendgrid api key --> so unauthorized
 process.env.SENDGRID_API_KEY && sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-const sendFrom = process.env.SENDMAIL_FROM || "sender_name@domain.com";
-const sendTo = process.env.SENDMAIL_TO || "receiver_name@domain.com";
-//todo: .....@domain.com is forbidden
-//todo: be sure you got process env
 
 //@ts-ignore
 async function sendEmail(req, res) {
     try {
-        console.log("REQ.BODY", req.body);
+        console.log("Request.Body: ", req.body);
         await sendgrid.send({
-            to: sendTo, // Your email where you'll receive emails
-            from: sendFrom, // your website email address here
-            subject: `${req.body.subject}`,
-            html: `<div>${req.body.message}</div>`,
+            to: "pr.ibrahimsari@gmail.com", // Your email where you'll receive emails
+            from: {name: req.body.name, email: "pr.ibrahimsari@gmail.com"}, // your website email address here
+            replyTo: {name: req.body.name, email: req.body.email}, // your website email address here
+            subject: `[isari.me] ${req.body.subject} by [${req.body.name}]`,
+            html: `<div><b><u>CONTACT FORM MESSAGE:</u></b></div>
+                   <div><b>NAME:</b> ${req.body.name}</div>
+                   <div><b>SUBJECT:</b> ${req.body.subject}</div>
+                   <div><b>EMAIL:</b> ${req.body.email}</div>
+                   <div><b>MESSAGE:</b> ${req.body.message}</div>`,
         });
     } catch (error) {
-        // console.log(error);
         // @ts-ignore
-        return res.status(error.statusCode || 500).json({error: error.message});
+        console.error("async function sendEmail ERROR: ", error);
+        // @ts-ignore
+        return res.status(error.statusCode || 500).json({error: error});
     }
 
-    return res.status(200).json({error: ""});
+    return res.status(200).json({error: ""}); //todo: error? mı success mi?
 }
 
 export default sendEmail;
