d416e2b4f7e9e020a8ef901768228ec93472fca9
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-08 04:56:59 +0300

    edit res success and err messages

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -1,30 +1,36 @@
 import sendgrid from "@sendgrid/mail";
 
-process.env.SENDGRID_API_KEY && sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-
 //@ts-ignore
 async function sendEmail(req, res) {
-    try {
-        console.log("Request.Body: ", req.body);
-        await sendgrid.send({
+    //get sendgrid api key
+    process.env.SENDGRID_API_KEY && sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+    //show request body in console
+    console.log("Request.Body: ", req.body);
+
+    //send it
+    await sendgrid
+        .send({
             to: "pr.ibrahimsari@gmail.com", // Your email where you'll receive emails
             from: {name: req.body.name, email: "pr.ibrahimsari@gmail.com"}, // your website email address here
-            replyTo: {name: req.body.name, email: req.body.email}, // your website email address here
+            replyTo: {name: req.body.name, email: req.body.email},
             subject: `[isari.me] ${req.body.subject} by [${req.body.name}]`,
             html: `<div><b><u>CONTACT FORM MESSAGE:</u></b></div>
                    <div><b>NAME:</b> ${req.body.name}</div>
                    <div><b>SUBJECT:</b> ${req.body.subject}</div>
                    <div><b>EMAIL:</b> ${req.body.email}</div>
                    <div><b>MESSAGE:</b> ${req.body.message}</div>`,
+        })
+        .then((response) => {
+            console.log("Sendgrid.send SUCCESS: ", response[0].headers);
+            return res.status(response[0].statusCode).json({success: response});
+        })
+        .catch((error) => {
+            // @ts-ignore
+            console.error("Sendgrid.send ERROR: ", error);
+            // @ts-ignore
+            return res.status(error.statusCode || 500).json({error: error});
         });
-    } catch (error) {
-        // @ts-ignore
-        console.error("async function sendEmail ERROR: ", error);
-        // @ts-ignore
-        return res.status(error.statusCode || 500).json({error: error});
-    }
-
-    return res.status(200).json({error: ""}); //todo: error? mı success mi?
 }
 
 export default sendEmail;
